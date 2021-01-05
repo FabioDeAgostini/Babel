1a922dd701a1a4d3e72515eb2965310eba3471e0
MAFFEIS\f.deagostini <f.eagostini@maffeis.it>
2021-01-05 16:27:18 +0100

    let's start

---
+++ Babel/BabelRevitAddin/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace BabelRevitAddin
+{
+    /*
+     * setup translate https://cloud.google.com/translate/docs/setup?hl=it 
+     * pricing and quota: https://cloud.google.com/translate/pricing
+     * translate api guide: https://cloud.google.com/translate/docs/reference/rest/?apix=true
+     * https://cloud.google.com/translate/docs/basic/translating-text?hl=it#translate_text_with_model-csharp
+     * 
+     * 
+     */
+
+    public class Babel : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
